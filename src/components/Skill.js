f8fe21847cd30e0ebf06ec91ed43c6b0012f0235
MhankBarBar <royani7896@gmail.com>
2021-10-16 10:21:04 +0700

    anjc

--- src/components/Skill.js
+++ src/components/Skill.js
@@ -42,7 +42,7 @@ const Skill = () => {
                                         loading ? renderSkeleton() : (
                                             config.skills.map((skill, index) => (
                                                 <div key={index} className="m-1 text-xs inline-flex items-center font-bold leading-sm uppercase px-3 py-1 badge-primary bg-opacity-75 rounded-full">
-                                                	index == 0 ?
+                                                	{index == 0 ?
                                                 	<div>
                                                 		<DiPython className="mr-2"/>
                                                 	</div>
@@ -61,10 +61,10 @@ const Skill = () => {
                                                 	:
                                                 	<div>
                                                 		<DiPython className="mr-2"/>
+                                                	</div>}
+                                                	<div className="text-base-content-important">
+                                                    	{skill}
                                                 	</div>
-                                                <div className="text-base-content-important">
-                                                    {skill}
-                                                </div>
                                                 </div>
                                             ))
                                         )
